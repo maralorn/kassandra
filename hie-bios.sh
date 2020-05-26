c7eee19b16d6767b6c8be64d1f6d5aca7e2a5d29
Malte Brandy <malte.brandy@maralorn.de>
2020-05-26 18:12:29 +0200

    Improve hie-bios file

--- hie-bios.sh
+++ hie-bios.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 echo "Outputing ghci config to $HIE_BIOS_OUTPUT" > hie-output
-ob internal export-ghci-configuration > "$HIE_BIOS_OUTPUT"
+ob internal export-ghci-configuration --no-interpret .obelisk/impl > "$HIE_BIOS_OUTPUT"
