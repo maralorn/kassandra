69728f26917c7b6ea08a32136af9e74cf29a9b87
Malte Brandy <malte.brandy@maralorn.de>
2020-11-21 00:33:44 +0100

    Disable config check for now

--- standalone/src/Kassandra/Standalone.hs
+++ standalone/src/Kassandra/Standalone.hs
@@ -23,9 +23,9 @@ standalone = do
   log Info "Started kassandra"
   log Debug "Writing Types file"
   writeDeclarations
-  log Debug "Loading Config"
-  config <- readConfig Nothing
-  print config
+  --log Debug "Loading Config"
+  --config <- readConfig Nothing
+  --print config
   log Debug "Loaded Config"
   callbackSlot <- newEmptyMVar
   race_
