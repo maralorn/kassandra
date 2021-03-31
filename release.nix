a711e41372c297f5deea04b3f374da2f8fa827d6
Malte Brandy <malte.brandy@maralorn.de>
2021-04-01 00:04:49 +0200

    Try to fix iCalendar

--- release.nix
+++ release.nix
@@ -1,18 +1,27 @@
-{ pkgs ? import (import nix/sources.nix).nixpkgs { } }:
+{ pkgs ? import (import nix/sources.nix).nixpkgs {} }:
 let
-  kassandra = pkgs.haskellPackages.callCabal2nix "kassandra" ./kassandra { };
+  haskellPackages = pkgs.haskellPackages.extend (
+    self: super: {
+      iCalendar = overrideCabal (doJailbreak (unmarkBroken super.iCalendar)) {
+        preConfigure = ''substituteInPlace iCalendar.cabal --replace "network >=2.6 && <2.7" "network -any"'';
+        #configureFlags = [ "--allow-newer=network" ]; # try this on ghc 9.0
+      };
+    }
+  );
+  kassandra = pkgs.haskellPackages.callCabal2nix "kassandra" ./kassandra {};
   standalone = pkgs.haskellPackages.callCabal2nix "standalone" ./standalone {
     inherit kassandra;
   };
-  reflex-platform = import ./. { };
-in {
+  reflex-platform = import ./. {};
+in
+{
   lib = kassandra;
   app = standalone;
   server = reflex-platform.exe;
-  android = pkgs.runCommand "kassandra-android-apk" { } ''
+  android = pkgs.runCommand "kassandra-android-apk" {} ''
     mkdir -p $out
     cp ${reflex-platform.android.frontend}/android-app-release-unsigned.apk $out/de.maralorn.kassandra_${
-      import ./code.nix
-    }.apk
+  import ./code.nix
+  }.apk
   '';
 }
