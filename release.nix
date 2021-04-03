c2d4629488231884a175828e5bb4577d308f69f5
Malte Brandy <malte.brandy@maralorn.de>
2021-04-03 13:18:56 +0200

    Bump nix deps

--- release.nix
+++ release.nix
@@ -1,26 +1,17 @@
 { pkgs ? import (import nix/sources.nix).nixpkgs {} }:
 let
-  inherit (pkgs.haskell.lib) overrideCabal doJailbreak unmarkBroken;
+  #inherit (pkgs.haskell.lib) overrideCabal doJailbreak unmarkBroken;
   haskellPackages = pkgs.haskellPackages.extend (
     self: super: {
-      iCalendar = overrideCabal (doJailbreak (unmarkBroken super.iCalendar)) {
-        preConfigure = ''substituteInPlace iCalendar.cabal --replace "network >=2.6 && <2.7" "network -any"'';
-        #configureFlags = [ "--allow-newer=network" ]; # try this on ghc 9.0
-      };
-      stm-containers = unmarkBroken super.stm-containers;
-      stm-hamt = unmarkBroken super.stm-hamt;
-      primitive-extras = unmarkBroken super.primitive-extras;
+      kassandra = self.callCabal2nix "kassandra" ./kassandra {};
+      standalone = self.callCabal2nix "standalone" ./standalone {};
     }
   );
-  kassandra = haskellPackages.callCabal2nix "kassandra" ./kassandra {};
-  standalone = haskellPackages.callCabal2nix "standalone" ./standalone {
-    inherit kassandra;
-  };
   reflex-platform = import ./. {};
 in
 {
-  lib = kassandra;
-  app = standalone;
+  lib = haskellPackages.kassandra;
+  app = haskellPackages.standalone;
   server = reflex-platform.exe;
   android = pkgs.runCommand "kassandra-android-apk" {} ''
     mkdir -p $out
