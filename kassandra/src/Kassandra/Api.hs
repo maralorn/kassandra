a445ef6ee6b2bb7ff3b6595578142a477c82c289
Malte Brandy <malte.brandy@maralorn.de>
2021-04-03 13:17:54 +0200

    Reformat

--- kassandra/src/Kassandra/Api.hs
+++ kassandra/src/Kassandra/Api.hs
@@ -3,17 +3,26 @@ module Kassandra.Api (
   SocketRequest (..),
 ) where
 
-import Kassandra.Config (UIConfig)
 import Kassandra.Calendar
+import Kassandra.Config (UIConfig)
 
 type SocketError = Text
 
-data SocketMessage = TaskUpdates (NonEmpty Task) | CalendarEvents (Seq CalendarEvent) | UIConfigResponse UIConfig | SocketError SocketError | ConnectionEstablished
+data SocketMessage
+  = TaskUpdates (NonEmpty Task)
+  | CalendarEvents (Seq CalendarEvent)
+  | UIConfigResponse UIConfig
+  | SocketError SocketError
+  | ConnectionEstablished
   deriving stock (Show, Read, Generic)
   deriving anyclass (ToJSON, FromJSON)
 makePrismLabels ''SocketMessage
 
-data SocketRequest = UIConfigRequest | AllTasks | CalenderRequest | ChangeTasks (NonEmpty Task)
-      deriving stock (Show, Read, Eq, Generic)
-      deriving anyclass (ToJSON, FromJSON)
+data SocketRequest
+  = UIConfigRequest
+  | AllTasks
+  | CalenderRequest
+  | ChangeTasks (NonEmpty Task)
+  deriving stock (Show, Read, Eq, Generic)
+  deriving anyclass (ToJSON, FromJSON)
 makePrismLabels ''SocketRequest
