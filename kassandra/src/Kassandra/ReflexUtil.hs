e465ca218da0020853f8b9a5149eba677a887320
Malte Brandy <malte.brandy@maralorn.de>
2021-04-11 17:20:09 +0200

    Fix smartSimpleList

--- kassandra/src/Kassandra/ReflexUtil.hs
+++ kassandra/src/Kassandra/ReflexUtil.hs
@@ -11,6 +11,7 @@ import qualified Data.Patch.Map as Patch
 import qualified Data.Patch.MapWithMove as Patch
 import qualified Data.Sequence as Seq
 import qualified Reflex as R
+import qualified Relude.Extra.Newtype as Newtype
 
 {- | Renders a list of widgets depending on a Dynamic list of inputs. This will
  call the widget constructor once per value in the list.
@@ -19,21 +20,30 @@ import qualified Reflex as R
  second copy of a same value) appears in the list.
 -}
 smartSimpleList ::
-  (R.Adjustable t m, R.PostBuild t m, Ord v) =>
+  forall t m v.
+  (R.Adjustable t m, R.PostBuild t m, Ord v, R.MonadHold t m, MonadFix m) =>
   (v -> m ()) ->
   R.Dynamic t (Seq v) ->
   m ()
 smartSimpleList widget listElements = do
   postBuild <- R.getPostBuild
-  let keyMap = Seq.foldMapWithIndex (curry one) <$> listElements
-      keyMapChange =
+  keyMap <- R.holdUniqDyn $ Seq.foldMapWithIndex (curry one) <$> listElements
+  let keyMapChange =
         R.attachWith
-          Patch.patchThatChangesMap
+          ((Newtype.under @(Map Int (Patch.NodeInfo Int v)) fixPatchMap .) . Patch.patchThatChangesMap)
           (R.current keyMap)
           (R.updated keyMap)
       initialKeyMap = Patch.patchMapWithMoveInsertAll <$> R.tag (R.current keyMap) postBuild
       keyMapEvents = keyMapChange <> initialKeyMap
   void $ R.mapMapWithAdjustWithMove (const widget) mempty keyMapEvents
+
+-- | A workaround for a bug in patchThatChangesMap in patch 0.0.3.2.
+fixPatchMap :: Map Int (Patch.NodeInfo Int v) -> Map Int (Patch.NodeInfo Int v)
+fixPatchMap inputMap = appEndo setMoves . fmap (Patch.nodeInfoSetTo Nothing) $ inputMap
+ where
+  setMoves = Map.foldMapWithKey f inputMap
+  f to' (Patch.NodeInfo (Patch.From_Move from) _) = Endo $ Map.adjust (Patch.nodeInfoSetTo (Just to')) from
+  f _ _ = mempty
 
 keyDynamic ::
   forall t k v.
