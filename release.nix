94383ba04052fdfff526c3a2295d14b5b9042f47
Malte Brandy <malte.brandy@maralorn.de>
2021-09-03 19:08:39 +0200

    Reformat nix files

--- release.nix
+++ release.nix
@@ -1,22 +1,19 @@
-{ pkgs ? import (import nix/sources.nix).nixpkgs {} }:
+{ pkgs ? import (import nix/sources.nix).nixpkgs { } }:
 let
-  #inherit (pkgs.haskell.lib) overrideCabal doJailbreak unmarkBroken;
   haskellPackages = pkgs.haskellPackages.extend (
     self: super: {
-      kassandra = self.callCabal2nix "kassandra" ./kassandra {};
-      standalone = self.callCabal2nix "standalone" ./standalone {};
+      kassandra = self.callCabal2nix "kassandra" ./kassandra { };
+      standalone = self.callCabal2nix "standalone" ./standalone { };
     }
   );
-  reflex-platform = import ./. {};
+  reflex-platform = import ./. { };
 in
 {
   lib = haskellPackages.kassandra;
   app = haskellPackages.standalone;
   server = reflex-platform.exe;
-  android = pkgs.runCommand "kassandra-android-apk" {} ''
+  android = pkgs.runCommand "kassandra-android-apk" { } ''
     mkdir -p $out
-    cp ${reflex-platform.android.frontend}/android-app-release-unsigned.apk $out/de.maralorn.kassandra_${
-  import ./code.nix
-  }.apk
+    cp ${reflex-platform.android.frontend}/android-app-release-unsigned.apk $out/de.maralorn.kassandra_${import ./code.nix}.apk
   '';
 }
--- shell.nix
+++ shell.nix
@@ -1,1 +1,1 @@
-(import ./. {}).shells.ghc
+(import ./. { }).shells.ghc
